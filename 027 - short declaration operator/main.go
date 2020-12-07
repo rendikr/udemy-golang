c7d3c891c238e286ea286d93f21fd3dcc30ab8ce
Rendi K <rendi.krinaldi@gmail.com>
2020-12-07 21:34:21 +0700

    short declaration operator

---
+++ 027 - short declaration operator/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42 // declare a variable "x" and assign a value to the variable
+	fmt.Println(x)
+
+	x = 99 // assign a value to a specific variable
+	fmt.Println(x)
+
+	y := 100 + 24 // declare a variable "y" and assign a value to the variable. The value in this line is an expression
+	fmt.Println(y)
+
+	z := "Bond, James"
+	fmt.Println(z)
+}
