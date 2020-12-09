54fc99d23f26cc4a5202147c9b86bb8e174a4e7c
Rendi K <rendi.krinaldi@gmail.com>
2020-12-10 04:00:24 +0700

    fmt package

---
+++ 031 - fmt package/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// declare a variable to be of a certain type
+var y int = 42
+
+func main() {
+	// godoc.org/fmt
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+	fmt.Printf("%b\n", y)
+	fmt.Printf("%x\n", y)
+	fmt.Printf("%#x\n", y)
+
+	y = 911
+	fmt.Printf("%#x\n", y)
+	fmt.Printf("%#x\t%b\t%x\n", y, y, y)
+
+	s := fmt.Sprintf("%#x\t%b\t%x\n", y, y, y)
+	fmt.Println(s)
+	fmt.Printf("%v", y)
+}
