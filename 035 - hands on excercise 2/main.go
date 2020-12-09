29af8e3e6654508706f6b0dc35409935b4848d88
Rendi K <rendi.krinaldi@gmail.com>
2020-12-10 04:22:40 +0700

    hands on exercise #2

---
+++ 035 - hands on excercise 2/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+// declare a variable to be of a certain type
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
