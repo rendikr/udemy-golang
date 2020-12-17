eee09a4c3061e11b7881fdcb9d2943b2cc9e2303
Rendi K <rendi.krinaldi@gmail.com>
2020-12-17 18:12:01 +0700

    loop - nesting loop

---
+++ 057 - loop - nesting loop/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// how to write an iteration
+	// for init; condition; post {}
+
+	for i := 0; i <= 10; i++ {
+		fmt.Printf("The outer loop: %d\n", i)
+		for j := 0; j < 3; j++ {
+			fmt.Printf("\t\tThe inner loop: %d\n", j)
+		}
+		fmt.Printf("\n")
+	}
+}
