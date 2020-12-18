a09d3c96d4183928f6a0ee23da626f271beb859d
Rendi K <rendi.krinaldi@gmail.com>
2020-12-18 18:46:24 +0700

    loop - printing ascii

--- 060 - loop - printing ascii/main.go
+++ 060 - loop - printing ascii/main.go
@@ -3,24 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	x := 83 / 40
-	y := 83 % 40
-
-	fmt.Println(x, y)
-
-	i := 1
-	for {
-		if i > 100 {
-			break
-		}
-
-		if i%2 != 0 {
-			i++
-			continue
-		}
-
-		fmt.Println(i)
-		i++
+	for i := 33; i <= 122; i++ {
+		fmt.Printf("%v\t%#U\n", i, i)
 	}
 	fmt.Println("done.")
 }
