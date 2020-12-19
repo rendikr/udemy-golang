b72f2f6d81dbf83220a9762b769438813ca3e21d
Rendi K <rendi.krinaldi@gmail.com>
2020-12-20 06:46:17 +0700

    fix typo

--- 066 - conditional logic operators/main.go
+++ 066 - conditional logic operators/main.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("true && true\t %v\n", true && true)
-	fmt.Print("true && false\t %v\n", true && false)
-	fmt.Print("true || true\t %v\n", true || true)
-	fmt.Print("true || false\t %v\n", true || false)
-	fmt.Print("!true\t %v\n", !true)
+	fmt.Printf("true && true\t %v\n", true && true)
+	fmt.Printf("true && false\t %v\n", true && false)
+	fmt.Printf("true || true\t %v\n", true || true)
+	fmt.Printf("true || false\t %v\n", true || false)
+	fmt.Printf("!true\t %v\n", !true)
 
 	fmt.Println("done.")
 }
