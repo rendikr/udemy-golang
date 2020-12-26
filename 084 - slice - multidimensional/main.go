38a14560f266e1855aca38d452292c8bf43ccb58
Rendi K <rendi.krinaldi@gmail.com>
2020-12-26 19:09:55 +0700

    slice - multidimensional

---
+++ 084 - slice - multidimensional/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	jb := []string{"James", "Bond", "Chocolate", "Martini"}
+	fmt.Println(jb)
+
+	mp := []string{"Miss", "Moneypenny", "Strawberry", "Gin"}
+	fmt.Println(mp)
+
+	xp := [][]string{jb, mp}
+	fmt.Println(xp)
+}
