979e8582cd0ce523090a4c81260f16e5e718b3ce
Rendi K <rendi.krinaldi@gmail.com>
2020-12-26 18:22:13 +0700

    composite literal / slice

---
+++ 078 - composite literal/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	// x := type{values} // composite literal
+	x := []int{4, 5, 7, 8, 42}
+	fmt.Println(x)
+}
+
+// a SLICE allows to group together VALUES of the same TYPE
