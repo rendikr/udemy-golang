faa5a341b8ef77920310da85f3874415671caca2
Rendi K <rendi.krinaldi@gmail.com>
2020-12-27 20:47:02 +0700

    hands on exercise #24

--- 090 - hands on exercise #24/main.go
+++ 090 - hands on exercise #24/main.go
@@ -4,8 +4,16 @@ import "fmt"
 
 func main() {
 	x := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
-	fmt.Println(x[:5])
-	fmt.Println(x[5:])
-	fmt.Println(x[2:7])
-	fmt.Println(x[1:6])
+
+	x = append(x, 52)
+	fmt.Println(x)
+
+	x = append(x, 53, 54, 55)
+	fmt.Println(x)
+
+	y := []int{56, 57, 58, 59, 60}
+	fmt.Println(y)
+
+	x = append(x, y...)
+	fmt.Println(x)
 }
