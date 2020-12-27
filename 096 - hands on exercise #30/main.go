150d16cad7c90ef9ac91d0104f166324e8fc19f9
Rendi K <rendi.krinaldi@gmail.com>
2020-12-27 21:19:05 +0700

    hands on exercise #30

---
+++ 096 - hands on exercise #30/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string][]string{
+		"bond_james": []string{
+			"Shaken, not stirred", "Martinis", "Women",
+		},
+		"moneypenny_miss": []string{
+			"James Bond", "Literature", "Computer Science",
+		},
+		"chiffre_le": []string{
+			"Cards", "Money", "Stock",
+		},
+	}
+
+	m["fleming_ian"] = []string{
+		"Steaks", "Cigar", "Espionage",
+	}
+
+	for k, v := range m {
+		fmt.Println("This is the record for", k)
+		for i, val := range v {
+			fmt.Println("\t", i, val)
+		}
+	}
+}
